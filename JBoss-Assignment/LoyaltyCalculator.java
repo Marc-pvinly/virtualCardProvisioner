1b261d0c8b49b8d046bbeae92a55e40f5cf6e129
Marc Pvinly <smayowa689@example.com>
2026-09-22 19:08:23 +0100

    code update

--- JBoss-Assignment/LoyaltyCalculator.java
+++ JBoss-Assignment/LoyaltyCalculator.java
@@ -1,156 +1,270 @@
-import java.math.BigDecimal;
-import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.*;
 
+public class LoyaltyCalculator {
+    public static void main(String[] args) {
+        Patronize sale = new Patronize();
+        Redeem convertPoint = new Redeem(); // create object for class redeem
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("-------------------------------Bolaji Gas Land -----------------------------------------");
+        System.out.println(
+                "--------------------------Welcome to Bolaji Gas Land---------------------------------------\n");
 
-public class LoyaltyCalculator{
-    public static void main(String[] args){
-        Customer test = new Customer();
-        test.customer1();
+        System.out.println("------------------------SALES-------------------------------------\n");
+        sale.day1();
+
+        System.out.println("------------------------Day2-----------------------------------\n");
+        sale.day2();
+
+        System.out.println("--------------------------------Redeem Point-------------------------------");
+        int currentPoint = sale.getNewEarnPoint();
+        convertPoint.redeem(currentPoint);
+
     }
+}
+
+class Patronize {
+    private static final int VALID_MONTH = 3;
+    Multiplier calculate = new Multiplier();
+
+    int day1EarnPoint;
+    int day2EarnPoint;
+    int newEarnPoint;
+
+    public void day1() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your name:");
+        String name = input.nextLine();
+
+        System.out.println("---------------------Welcome to Bolaji Gas Land " + name.toUpperCase()
+                + "-------------------------------\n");
+        System.out.println("Enter the amount of Gas you want to buy: ");
+
+        // handle inputMismatchException
+        if (input.hasNextInt()) {
+            int gasAmount = input.nextInt();
+
+            if (gasAmount <= 3000) {
+                day1EarnPoint = calculate.tier1(gasAmount);
+                System.out.println("\n");
+                System.out.println("Thanks for Patronizing us! " + name.toUpperCase());
+                System.out.println("\n");
+                System.out.println("You bought " + gasAmount + "# Gas");
+                System.out.println("\n");
+                System.out.println("Your gas amount is categorize to tier 1:--- you earn: " + day1EarnPoint
+                        + "Point ------------------Patronize us more to get more point-------------------------");
+
+                // EXPIRE DATE
+                PointTrackingInfo track = new PointTrackingInfo(day1EarnPoint, VALID_MONTH);
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("Note: These points will expire on: " + track.getFormattedExpired());
+
+            } else if (gasAmount > 3000 && gasAmount <= 10000) {
+                day1EarnPoint = calculate.tier2(gasAmount);
+                System.out.println("\n");
+                System.out.println("Thanks for your Patroning us! " + name.toUpperCase());
+                System.out.println("\n");
+                System.out.println("You bought " + gasAmount + "# Gas");
+                System.out.println("\n");
+                System.out.println("Your gas amount is categorize to tier 2:---  You earn: " + day1EarnPoint
+                        + "Point ------------------Patronize us more to get more point-------------------------");
+                // EXPIRE DATE
+                PointTrackingInfo track = new PointTrackingInfo(day1EarnPoint, VALID_MONTH);
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("Note: These points will expire on: " + track.getFormattedExpired());
+
+            } else {
+                day1EarnPoint = calculate.tier3(gasAmount);
+                System.out.println("\n");
+                System.out.println("Thanks for Patronizing us! " + name.toUpperCase());
+                System.out.println("\n");
+                System.out.println("You bought " + gasAmount + "# Gas");
+                System.out.println("\n");
+                System.out.println("Your gas amount is categorize to tier 3: ---you earn: " + day1EarnPoint
+                        + "Point ------------------Patronize us more to get more point-------------------------");
+
+                // EXPIRE DATE
+                PointTrackingInfo track = new PointTrackingInfo(day1EarnPoint, VALID_MONTH);
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("Note: These points will expire on: " + track.getFormattedExpired());
+
+            }
+
+        } else {
+            System.out.println("\n[ERROR] Invalid input type! Please enter only numbers .");
+        }
+    }
+
+    public void day2() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your name:");
+
+        String name = input.nextLine();
+
+        System.out.println("---------------------Welcome to Bolaji Gas Land " + name.toUpperCase()
+                + "-------------------------------\n");
+        System.out.println("Enter the amount of Gas you want to buy: ");
+
+        // Handle InputMismatchException
+        if (input.hasNextInt()) {
+
+            int gasAmount = input.nextInt();
+
+            if (gasAmount <= 3000) {
+                day2EarnPoint = calculate.tier1(gasAmount);
+                newEarnPoint = day1EarnPoint + day2EarnPoint;
+                System.out.println("\n");
+                System.out.println("Thanks for Patronizing us! " + name.toUpperCase());
+
+                System.out.println("\n");
+
+                System.out.println("You bought " + gasAmount + "# Gas");
+                System.out.println("\n");
+                System.out.println("Your gas amount is categorize to tier 1:--- you earn: " + day2EarnPoint
+                        + "Point ------------------Patronize us more to get more point-------------------------\n");
+
+                System.out.println("Your TotalPoint is: " + newEarnPoint + "point");
+
+                // EXPIRE DATE
+                PointTrackingInfo track = new PointTrackingInfo(day2EarnPoint, VALID_MONTH);
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("Note: These points will expire on: " + track.getFormattedExpired());
+
+            } else if (gasAmount > 3000 && gasAmount <= 10000) {
+                day2EarnPoint = calculate.tier2(gasAmount);
+                newEarnPoint = day1EarnPoint + day2EarnPoint;
+                System.out.println("\n");
+                System.out.println("Thanks for Patronizing us! " + name.toUpperCase());
+                System.out.println("\n");
+                System.out.println("You bought " + gasAmount + "# Gas");
+                System.out.println("\n");
+                System.out.println("Your gas amount is categorize to tier 2:---  You earn: " + day2EarnPoint
+                        + "Point ------------------Patronize us more to get more point-------------------------\n");
+                System.out.println("Your TotalPoint is: " + newEarnPoint + "point");
+
+                // EXPIRE DATE
+                PointTrackingInfo track = new PointTrackingInfo(day2EarnPoint, VALID_MONTH);
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("Note: These points will expire on: " + track.getFormattedExpired());
+
+            } else {
+                day2EarnPoint = calculate.tier3(gasAmount);
+                newEarnPoint = day1EarnPoint + day2EarnPoint;
+                System.out.println("\n");
+                System.out.println("Thanks for Patronizing us! " + name.toUpperCase());
+                System.out.println("\n");
+                System.out.println("You bought " + gasAmount + "# Gas");
+                System.out.println("\n");
+                System.out.println("Your gas amount is categorize to tier 3: ---you earn: " + day2EarnPoint
+                        + "Point ------------------Patronize us more to get more point-------------------------\n");
+                System.out.println("Your TotalPoint is: " + newEarnPoint + "point");
+
+                // EXPIRE DATE
+                PointTrackingInfo track = new PointTrackingInfo(day2EarnPoint, VALID_MONTH);
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("Note: These points will expire on: " + track.getFormattedExpired());
+
+            }
+
+        } else {
+            System.out.println("\n[ERROR] Invalid input type! Please enter only numbers .");
+        }
+
+    }
+
+    public int getNewEarnPoint() {
+        return this.newEarnPoint;
+    }
+
 }
 
 class Multiplier {
-    public double tier1() {
-        //we are using 1% for tier 1
-        BigDecimal percentage = new BigDecimal("1");
-        BigDecimal earningRatio = percentage.divide( new BigDecimal("100"));
-        return earningRatio.doubleValue();
+    public int tier1(int gasAmount) {
+        System.out.println(" We use 2% to calculate tier 1, that's 2/100 = 0.02");
+        double earnRatio = 0.02;
+        int accruePoint = (int) (gasAmount * earnRatio);
+        return accruePoint;
     }
-    public double tier2() {
-        //we are using 1.5% for tier 2
-        BigDecimal percentage = new BigDecimal("1.5");
-        BigDecimal earningRatio = percentage.divide( new BigDecimal("100"));
-        return earningRatio.doubleValue();
+
+    public int tier2(int gasAmount) {
+        System.out.println("We use 4% to calculate tier 2, that's 4/100 = 0.04");
+        double earnRatio = 0.04;
+        int accruePoint = (int) (gasAmount * earnRatio);
+        return accruePoint;
     }
-    public double tier3() {
-        //we are using 5% for tier 3
-        BigDecimal percentage = new BigDecimal("5");
-        BigDecimal earningRatio = percentage.divide( new BigDecimal("100"));
-        return earningRatio.doubleValue();
+
+    public int tier3(int gasAmount) {
+        System.out.println("We use 6% to calculate tier 3 that's 6/100 = 0.06");
+        double earnRatio = 0.06;
+        int accruePoint = (int) (gasAmount * earnRatio);
+        return accruePoint;
     }
 
 }
 
+class PointTrackingInfo {
+    private final int points;
+    private final LocalDate dateEarned;
+    private final LocalDate expiryDate;
 
-
-
-class Customer extends Multiplier{
-
-    public int customer1(){
-    Scanner input = new Scanner(System.in);
-    String name = "Sam";
-    double accountBalance = 200000.00;
-    String tier = "Tier 1";
-    int accruePoint = 0;
-    int maximiumRedeemPoint = 300;
-
-    Multiplier multiplier = new Multiplier();
-    double tier1EarningRatio = multiplier.tier1();
-    
-    try{
-    System.out.println("Enter your Transaction Amount: ");
-    double transaction = input.nextDouble();
-    if (transaction > accountBalance){
-        System.out.println("Insufficient Balance");
-        return 0;
-    } 
-
-    else  if(transaction < 1000){
-        double newBalance = accountBalance - transaction;
-        System.out.println("Thanks for making a transaction with us. Your new balance is: " + newBalance);
-        System.out.println("Transaction successful but you did not earn any point. Make sure you make transaction of 1000 or above to be eligible for point.");
-        
-        accruePoint = 0; 
-        return accruePoint;
-
+    // create constructor
+    public PointTrackingInfo(int points, int validMonths) {
+        this.points = points;
+        this.dateEarned = LocalDate.now();
+        this.expiryDate = this.dateEarned.plusMonths(validMonths);
     }
-    
-    else{
-        double newBalance = accountBalance - transaction;
-        accruePoint = (int) (transaction * tier1EarningRatio);
-        System.out.println("Thanks for making a transaction with us. Your new balance is: " + newBalance);
-        System.out.println("You earned " + accruePoint + " points!");
 
-        if(accruePoint < maximiumRedeemPoint){
-            System.out.println("You are not eligible to convert your point wait still it reach 300point");
+    public int getPoints() {
+        return points;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    // check if the point is still valid
+
+    public boolean isExpired() {
+        return LocalDate.now().isAfter(expiryDate);
+    }
+
+    public String getFormattedExpired() {
+        return expiryDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+    }
+}
+
+class Redeem {
+    private static final int MAX_REDEEM_POINT_THRESHOLD = 300;
+
+    public void redeem(int newEarnPoint) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Did you want to redeem your point: (yes/no)");
+        String userInput = input.nextLine();
+        if (userInput.equalsIgnoreCase("yes")) {
+
+            System.out.println("Currently your TotalPoint: " + newEarnPoint + "point");
+            System.out.println("\n");
+
+            // Redeem Point
+            if (newEarnPoint < MAX_REDEEM_POINT_THRESHOLD) {
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("You are not eligible to convert your points. Wait until it reaches 300 points.");
+
+            } else {
+                // i use 2point = 1#
+                int redeemAmount = newEarnPoint / 2;
+                System.out.println("---------------------------------------------------------\n");
+                System.out.println("You have successfully converted your points!");
+                System.out.println("You have #" + redeemAmount);
+
+            }
+
+        } else {
+            System.out.println("Thank for your Patronize! see you next time. ");
         }
-        else{
-            int redeem = accruePoint / 2 * 1;
-            System.out.println("You have successfuly convert your point!");
-            System.out.println("You have " + redeem + "#");
-        }
-        return accruePoint;
+
     }
 
-}catch(InputMismatchException e) {
-    System.out.println("Invalid Input!");
-    return  1;
-} finally{
-    input.close();
 }
-
-    }
-
-    public  int customer2(){
-    Scanner input = new Scanner(System.in);
-    String name = "Adigun taiwo";
-    double accountBalance = 400000.00;
-    String tier = "Tier 2";
-    int accruePoint = 0;
-
-    Multiplier multiplier = new Multiplier();
-    double tier2EarningRatio = multiplier.tier2();
-    
-    try{
-    System.out.println("Enter your Transaction Amount: ");
-    double transaction = input.nextDouble();
-
-  
-    if (transaction > accountBalance){
-        System.out.println("Insufficient Balance");
-        return 0;
-    } else 
-    if(transaction < 1000){
-        double newBalance = accountBalance - transaction;
-        System.out.println("Thanks for making a transaction with us. Your new balance is: " + newBalance);
-        System.out.println("Transaction successful but you did not earn any point. Make sure you make transaction of 1000 or above to be eligible for point.");
-        
-        accruePoint = 0; 
-        return accruePoint;
-    }else{
-        double newBalance = accountBalance - transaction;
-        accruePoint = (int) (transaction * tier2EarningRatio);
-        System.out.println("Thanks for making a transaction with us. Your new balance is: " + newBalance);
-        System.out.println("You earned " + accruePoint + " points!");
-        return accruePoint;
-    }
-
-      }catch(InputMismatchException e){
-        System.out.println("Invalid input!");
-        return 1;
-        
-    }
-
-    finally{
-        input.close();
-    }
-
-    // redeem point 
-
-
-    }
-
-
-
-
-
-   
-}
-
-
-
-
-
-
--- JBoss-Assignment/TransactionCategoryEngine.java
+++ JBoss-Assignment/TransactionCategoryEngine.java
@@ -4,130 +4,40 @@ public class TransactionCategoryEngine {
 
         public static void main(String[] args) {
 
+                
                 ArrayList<Transaction> transactions = new ArrayList<>();
 
                 // Groceries
                 transactions.add(new Transaction(
                                 "TXN001", "Shoprite", "POS SHOPRITE IBADAN",
-                                25000, "Groceries"));
+                                25000, Category.GROCERIES));
 
                 transactions.add(new Transaction(
                                 "TXN004", "FoodCo", "POS FOODCO IBADAN",
-                                18200, "Groceries"));
+                                18200, Category.GROCERIES ));
 
-                transactions.add(new Transaction(
-                                "TXN007", "SPAR", "SPAR SUPERMARKET",
-                                32000, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN010", "Justrite", "JUSTRITE SUPERMARKET",
-                                14800, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN013", "Market Square", "MARKET SQUARE IBADAN",
-                                21500, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN016", "Hubmart", "HUBMART SUPERMARKET",
-                                28700, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN019", "Next Cash & Carry", "NEXT CASH AND CARRY",
-                                35000, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN022", "Prince Ebeano", "PRINCE EBEANO SUPERMARKET",
-                                24600, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN025", "Mini Mart", "MINI MART IBADAN",
-                                7500, "Groceries"));
-
-                transactions.add(new Transaction(
-                                "TXN028", "FreshMart", "FRESHMART FOOD ITEMS",
-                                13200, "Groceries"));
+              
 
                 // Transport
                 transactions.add(new Transaction(
                                 "TXN002", "Bolt", "BOLT TRIP IBADAN",
-                                4500, "Transport"));
+                                4500, Category.TRANSPORT));
 
                 transactions.add(new Transaction(
                                 "TXN005", "Uber", "UBER TRIP",
-                                6000, "Transport"));
+                                6000, Category.TRANSPORT));
 
-                transactions.add(new Transaction(
-                                "TXN008", "ABC Transport", "ABC TRANSPORT TICKET",
-                                15000, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN011", "Bolt", "BOLT RIDE",
-                                3800, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN014", "GUO Transport", "GUO TRANSPORT TICKET",
-                                17500, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN017", "Peace Mass Transit", "PEACE MASS TRANSIT",
-                                13000, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN020", "Oando", "OANDO FUEL STATION",
-                                20000, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN023", "MRS", "MRS FILLING STATION",
-                                18500, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN026", "NNPC", "NNPC FUEL PURCHASE",
-                                30000, "Transport"));
-
-                transactions.add(new Transaction(
-                                "TXN029", "Local Taxi", "TAXI FARE IBADAN",
-                                3500, "Transport"));
+                
 
                 // Dining
                 transactions.add(new Transaction(
                                 "TXN003", "Chicken Republic", "POS CHICKEN REPUBLIC",
-                                8500, "Dining"));
+                                8500, Category.DINING));
 
                 transactions.add(new Transaction(
                                 "TXN006", "KFC", "KFC IBADAN",
-                                11500, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN009", "Mr Bigg's", "MR BIGGS FOOD",
-                                6500, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN012", "The Place", "THE PLACE RESTAURANT",
-                                12000, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN015", "Domino's Pizza", "DOMINOS PIZZA IBADAN",
-                                14500, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN018", "Tantalizers", "TANTALIZERS IBADAN",
-                                7800, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN021", "Foodies", "FOODIES RESTAURANT",
-                                9500, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN024", "Rhapsody's", "RHAPSODYS RESTAURANT",
-                                22000, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN027", "Bungalow Restaurant", "BUNGALOW RESTAURANT",
-                                16500, "Dining"));
-
-                transactions.add(new Transaction(
-                                "TXN030", "Mama's Kitchen", "MAMAS KITCHEN MEAL",
-                                5500, "Dining"));
+                                11500, Category.DINING));
+                
 
                 for (Transaction transaction : transactions) {
                         System.out.println(
@@ -144,14 +54,14 @@ public class TransactionCategoryEngine {
                 double transportTotalAmount = 0;
                 double diningTotalAmount = 0;
 
-                // --- FIXED: Create 'enter' variable outside try block ---
                 int enter = 0; 
-                try{
+                
                 Scanner input = new Scanner(System.in);
                 System.out.println("Enter 1: for Groceries 2: for Transport  3: for Dining ");
+                if(input.hasNextInt()){
 
                 enter = input.nextInt();
-                } catch (InputMismatchException e) {
+                }else {
                         System.out.println("\n[ERROR] Invalid input type! Please enter numbers only (1, 2, or 3).");
                               // Exits the main method gracefully instead of crashing
                 }
@@ -160,19 +70,19 @@ public class TransactionCategoryEngine {
 
                         switch (enter) {
                                 case 1:
-                                        if (transact.category.equalsIgnoreCase("Groceries")) {
+                                        if (transact.category.equals(Category.GROCERIES)) {
                                                 groceriesTotalAmount += transact.amount;
                                         }
                                         break;
 
                                 case 2:
-                                        if (transact.category.equalsIgnoreCase("Transport")) {
+                                        if (transact.category.equals(Category.TRANSPORT)) {
                                                 transportTotalAmount += transact.amount;
                                         }
                                         break;
 
                                 case 3:
-                                        if (transact.category.equalsIgnoreCase("Dining")) {
+                                        if (transact.category.equals(Category.DINING)) {
                                                 diningTotalAmount += transact.amount;
                                         }
                                         break;
@@ -196,20 +106,28 @@ public class TransactionCategoryEngine {
         }
 }
 
+
+enum Category{
+   GROCERIES,
+   TRANSPORT,
+   DINING
+        
+} 
+
 class Transaction {
         String transactionId;
         String merchant;
         String description;
         double amount;
-        String category;
-
+        Category category;
         // Constructor
-        public Transaction(String transactionId, String merchant, String description, double amount, String category) {
+        public Transaction(String transactionId, String merchant, String description, double amount, Category category) {
                 this.transactionId = transactionId;
                 this.merchant = merchant;
                 this.description = description;
                 this.amount = amount;
                 this.category = category;
+                
         }
 
 }
